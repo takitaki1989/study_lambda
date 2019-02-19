a9daee525e9a554186687abd70086d0009e00d5d
Yuhi Sekine <ysekine@autoc-one.jp>
2019-02-19 09:38:41 +0900

    初回コミット

---
+++ src/main/java/study_lambda/lambda/App.java
@@ -0,0 +1,18 @@
+package study_lambda.lambda;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	final static Logger log = Logger.getLogger(App.class.getName());
+    public static void main( String[] args )
+    
+    {
+    	
+    	log.error( "Hello World!" );
+    }
+}
